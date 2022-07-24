1028b84767c7689524a7239762fcc9081fc7a4db
qizhaohong <qizhaohong_sh@qq.com>
2022-07-24 17:02:21 +0800

    Rename oss to aliyun

---
+++ tsollu-starter-aliyun/src/main/java/com/tsollu/aliyun/TsolluAliyunAutoConfiguration.java
@@ -0,0 +1,11 @@
+package com.tsollu.aliyun;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author larry.qi
+ */
+@Configuration(proxyBeanMethods = false)
+public class TsolluAliyunAutoConfiguration {
+
+}
